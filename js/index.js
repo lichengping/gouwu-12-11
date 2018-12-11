f1cacbf8ded035ea917fcefca4a89235966d76a0
join cheng <13265463912@163.com>
2018-12-11 21:44:44 +0800

    购物车第一次

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(function(){
+
+    $('#cartDemo').fullpage({
+        navigation:true,
+        sectionsColor:['#f9dd67','#84a2d4','#ef674d','#ffeedd','#cf4759','#85d9ed','#8ac060','#84d9ed'],
+    });
+})
